cd2de494c912bf8e50ad92816bac16c6024e9b84
ejnAjaK3VgnnHBLk <charles.igel@s.stemk12.org>
2021-08-30 21:46:17 -0600

    test

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "cpu.hpp"
+
+int main() {
+    CPU cpu;
+    Mem mem;
+
+    cpu.Reset( mem );
+
+    /* Start - Inline test */
+    mem[0xFFFC] = cpu.INS_JSR; // Jump to first address in stack
+    mem[0xFFFD] = 0x01;
+    mem[0xFFFE] = 0x00;
+
+    mem[0x0001] = cpu.INS_LDX_ABS;  // We want the value in 0x42, and 2 is in the
+                                    // X register so 0x42-0x2 = 0x0040;
+    mem[0x0002] = 0x41;
+    mem[0x0003] = 0x0F;
+
+    mem[0x0F41] = 0x69;
+
+    /* End - Inline test */
+
+    cpu.Execute( 6, mem );
+    cpu.Execute( 4, mem);
+
+}
---
+++ src/mem.hpp
@@ -0,0 +1,34 @@
+#include <cassert>
+#include <cstdint>
+
+using Byte = uint8_t;
+using Word = uint16_t;
+
+const static unsigned int MAX_MEM = 1024 * 64;
+
+struct Mem {
+    Byte Data[MAX_MEM];
+
+    void Init() {
+        // Reset all of memory to 0's
+        for ( unsigned int i = 0; i < MAX_MEM; i++ )
+            Data[i] = 0;
+    }
+
+    Byte operator[] (unsigned int address)  const {
+        assert (address <= MAX_MEM);
+        return Data[address];
+    }
+
+    // Read one byte
+    Byte& operator[] (unsigned int address) {
+        assert (address <= MAX_MEM);
+        return Data[address];
+    }
+
+    void WriteWord(Word dta, unsigned int addr, unsigned int &cycles) {
+        Data[addr] = dta & 0xFF;
+        Data[addr+1] = (dta >> 8);
+        cycles -= 2;
+    }
+};
